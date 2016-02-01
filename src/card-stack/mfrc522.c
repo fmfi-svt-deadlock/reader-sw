6697b0b222b382eb279cd0a3cdcaf46aaa2d3a81
Adam Dej <dejko.a@gmail.com>
2016-02-01 23:32:12 +0100

    [WIP] [mfrc522] Fix reload reg value and CRC endianity

--- src/card-stack/mfrc522.c
+++ src/card-stack/mfrc522.c
@@ -29,73 +29,6 @@ typedef enum {
     PCD_CALCCRC         = 0x03
 } Mfrc522Command;
 
-typedef enum {
-    Reserved00          = 0x00,
-    CommandReg          = 0x01,
-    ComIEnReg           = 0x02,
-    DivlEnReg           = 0x03,
-    CommIrqReg          = 0x04,
-    DivIrqReg           = 0x05,
-    ErrorReg            = 0x06,
-    Status1Reg          = 0x07,
-    Status2Reg          = 0x08,
-    FIFODataReg         = 0x09,
-    FIFOLevelReg        = 0x0A,
-    WaterLevelReg       = 0x0B,
-    ControlReg          = 0x0C,
-    BitFramingReg       = 0x0D,
-    CollReg             = 0x0E,
-    Reserved01          = 0x0F,
-    Reserved10          = 0x10,
-    ModeReg             = 0x11,
-    TxModeReg           = 0x12,
-    RxModeReg           = 0x13,
-    TxControlReg        = 0x14,
-    TxASKReg            = 0x15,
-    TxSelReg            = 0x16,
-    RxSelReg            = 0x17,
-    RxThresholdReg      = 0x18,
-    DemodReg            = 0x19,
-    Reserved11          = 0x1A,
-    Reserved12          = 0x1B,
-    MfTxReg             = 0x1C,
-    MfRxReg             = 0x1D,
-    Reserved14          = 0x1E,
-    SerialSpeedReg      = 0x1F,
-    Reserved20          = 0x20,
-    CRCResultRegL       = 0x21,
-    CRCResultRegH       = 0x22,
-    Reserved21          = 0x23,
-    ModWidthReg         = 0x24,
-    Reserved22          = 0x25,
-    RFCfgReg            = 0x26,
-    GsNReg              = 0x27,
-    CWGsPReg            = 0x28,
-    ModGsPReg           = 0x29,
-    TModeReg            = 0x2A,
-    TPrescalerReg       = 0x2B,
-    TReloadRegH         = 0x2C,
-    TReloadRegL         = 0x2D,
-    TCounterValueRegH   = 0x2E,
-    TCounterValueRegL   = 0x2F,
-    Reserved30          = 0x30,
-    TestSel1Reg         = 0x31,
-    TestSel2Reg         = 0x32,
-    TestPinEnReg        = 0x33,
-    TestPinValueReg     = 0x34,
-    TestBusReg          = 0x35,
-    AutoTestReg         = 0x36,
-    VersionReg          = 0x37,
-    AnalogTestReg       = 0x38,
-    TestDAC1Reg         = 0x39,
-    TestDAC2Reg         = 0x3A,
-    TestADCReg          = 0x3B,
-    Reserved31          = 0x3C,
-    Reserved32          = 0x3D,
-    Reserved33          = 0x3E,
-    Reserved34          = 0x3F
-} MFRC522Register;
-
 static const SPIConfig mfrc522_spi_config = SPI_MFRC522_HAL_CONFIG;
 
 /*
@@ -142,6 +75,10 @@ void dlMfrc522DriverInit(void) {
 }
 
 void dlMfrc522Init(void) {
+    dlMfrc522Reset();
+
+    chThdSleepMicroseconds(40); // Oscillator start-up time
+
     // Set TAuto - timer starts automatically after the enf of transmission
     //     TPrescaler_Hi - set to 0x0D
     dlMfrc522WriteRegister(TModeReg, 0x8D);
@@ -152,7 +89,7 @@ void dlMfrc522Init(void) {
 
     // Set reload value to 0x1E. The timer will run for approx 15ms.
     dlMfrc522WriteRegister(TReloadRegH, 0x00);
-    dlMfrc522WriteRegister(TReloadRegL, 0x00);
+    dlMfrc522WriteRegister(TReloadRegL, 0x1E);
 
     // Set Force100ASK: Forces a 100% ASK modulation independent of the
     //                  ModGSPReg setting
@@ -163,6 +100,8 @@ void dlMfrc522Init(void) {
     //     PolMFin    : MFIN pin is active HIGH
     //     CRCPreset  : to 0x6363 as specified in ISO/IEC 14443
     dlMfrc522WriteRegister(ModeReg, 0x3D);
+
+    dlMfrc522AntennaOn();
 }
 
 #define dlMfrc522Command(command) (dlMfrc522WriteRegister(CommandReg, command))
@@ -183,7 +122,7 @@ uint16_t dlMfrc522CalculateCRCA(uint8_t *data, uint16_t n) {
         ;
     }
 
-    return (dlMfrc522ReadRegister(CRCResultRegH) << 8) | dlMfrc522ReadRegister(CRCResultRegL);
+    return (dlMfrc522ReadRegister(CRCResultRegH)) | (dlMfrc522ReadRegister(CRCResultRegL) << 8);
 }
 
 int16_t dlMfrc522Transceive(uint8_t *data_tx, uint16_t n_tx, uint8_t *data_rx, uint16_t n_rx) {
