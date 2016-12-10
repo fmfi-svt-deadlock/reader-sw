8633e42678c8784b449c03ffc618c6b7d938656c
Adam Dej <dejko.a@gmail.com>
2016-12-10 18:03:09 +0100

    [RFID Stack] MFRC522: implement trx of all frames (untested)!

--- hal/src/hal_mfrc522/hal_mfrc522_pcd_api.c
+++ hal/src/hal_mfrc522/hal_mfrc522_pcd_api.c
@@ -8,6 +8,8 @@
 
 #include "hal_mfrc522_internal.h"
 
+// ----------- COMMON MACROS -------------
+
 #define MEMBER_FUNCTION_CHECKS(inst)                                          \
     osalDbgCheck(inst != NULL);                                               \
     osalDbgCheck(((Pcd*)inst)->data != NULL);
@@ -18,6 +20,80 @@
 // TODO counterintuitive!
 #define CHECK_STATE(condition)                                                \
     if (condition) { return PCD_BAD_STATE; }
+
+// ----------- COMMON TRANSMISSION ROUTINES -------------
+
+static void prepare_transceive(Mfrc522Driver *mdp) {
+    // Flush FIFO
+    mfrc522_write_register(mdp, FIFOLevelReg, (1 << FIFOLevelReg_FlushBuffer));
+
+    // Clear interrupt bits
+    // Select all interrupts, bit Set1 is zero. This will unset all selected
+    // interrupts.
+    mfrc522_write_register(mdp, ComIrqReg, 0xFF & ~(1 << ComIrqReg_Set1));
+    mdp->interrupt_pending = false;
+    // Enable interrupts: Rx complete, Timeout and Error
+    mfrc522_set_register_bits(mdp, ComIEnReg,
+                      (1 << ComIEnReg_RxIEn) |
+                      (1 << ComIEnReg_ErrIEn));
+
+
+    mfrc522_command(mdp, Command_Transceive);
+}
+
+static msg_t wait_for_response(Mfrc522Driver *mdp, uint32_t timeout_us) {
+    osalSysLock();
+    msg_t message;
+    if (!mdp->interrupt_pending) {
+        message = osalThreadSuspendTimeoutS(&mdp->tr,
+                                            OSAL_US2ST(timeout_us));
+    }
+    mdp->interrupt_pending = false;
+    osalSysUnlock();
+}
+
+static void cleanup_transceive(Mfrc522Driver *mdp) {
+    // Reset bit-oriented adjustments and clear 'Start Send' flag
+    mfrc522_write_register(mdp, BitFramingReg, 0);
+
+    // Clear interrupt bits and disable previously enabled interrupts
+    mfrc522_clear_register_bits(mdp, ComIEnReg,
+                        (1 << ComIEnReg_RxIEn) |
+                        (1 << ComIEnReg_ErrIEn));
+    mfrc522_write_register(mdp, ComIrqReg, 0xFF & ~(1 << ComIrqReg_Set1));
+}
+
+static pcdresult_t handle_response(Mfrc522Driver *mdp,
+                                   msg_t message,
+                                   uint16_t *resp_length,
+                                   bool collisions_possible) {
+    if (message == MSG_TIMEOUT) {
+        return PCD_OK_TIMEOUT;
+    }
+
+    if (message != MFRC522_MSG_INTERRUPT) {
+        // OK, this should not have happened. Fail-fast.
+        osalSysHalt("Unexpected wakeup message");
+    }
+
+    // Handle possible error
+    if (!(mfrc522_read_register(mdp, ComIrqReg) & (1 << ComIrqReg_RxIRq))) {
+        // This was not a 'receive complete' interrupt
+        // TODO more thorough error handling
+        return PCD_ERROR;
+    }
+
+    // TODO if collisions are possible handle them!
+
+    mdp->resp_read_bytes = 0;
+    mdp->resp_length = mfrc522_read_register(mdp, FIFOLevelReg);
+    *resp_length = mdp->resp_length;
+    mfrc522_read_register_burst(mdp, FIFODataReg, mdp->response, mdp->resp_length);
+
+    return PCD_OK;
+}
+
+// ----------- API FUNCTIONS -------------
 
 pcdstate_t mfrc522GetStateAB(void *inst) {
     MEMBER_FUNCTION_CHECKS(inst);
@@ -123,72 +199,22 @@ pcdresult_t mfrc522TransceiveShortFrameA(void *inst, uint8_t data,
     CHECK_STATE(mdp->state != PCD_READY);
 
     mdp->state = PCD_ACTIVE;
+    prepare_transceive(mdp);
 
-    // Flush FIFO
-    mfrc522_write_register(mdp, FIFOLevelReg, (1 << FIFOLevelReg_FlushBuffer));
-
-    // Write data
+    // Write data, set Start Send and transmit 7 bits
     mfrc522_write_register(mdp, FIFODataReg, data);
+    mfrc522_write_register(mdp, BitFramingReg,
+                           (7 << BitFramingReg_TxLastBits) |
+                           (1 << BitFramingReg_StartSend));
 
-    // Clear interrupt bits
-    // Select all interrupts, bit Set1 is zero. This will unset all selected
-    // interrupts.
-    mfrc522_write_register(mdp, ComIrqReg, 0xFF & ~(1 << ComIrqReg_Set1));
-    mdp->interrupt_pending = false;
-    // Enable interrupts: Rx complete, Timeout and Error
-    mfrc522_set_register_bits(mdp, ComIEnReg,
-                      (1 << ComIEnReg_RxIEn) |
-                      (1 << ComIEnReg_ErrIEn));
+    msg_t message = wait_for_response(mdp, timeout_us);
 
-
-    mfrc522_command(mdp, Command_Transceive);
-    // Start Send and transmit 7 bits
-    mfrc522_write_register(mdp, BitFramingReg, (7 << BitFramingReg_TxLastBits) |
-                                       (1 << BitFramingReg_StartSend));
-
-    osalSysLock();
-    msg_t message;
-    if (!mdp->interrupt_pending) {
-        message = osalThreadSuspendTimeoutS(&mdp->tr,
-                                            OSAL_US2ST(timeout_us));
-    }
-    mdp->interrupt_pending = false;
-    osalSysUnlock();
-
-    // Reset bit-oriented adjustments and clear 'Start Send' flag
-    mfrc522_write_register(mdp, BitFramingReg, 0);
-
-    // Clear interrupt bits and disable previously enabled interrupts
-    mfrc522_clear_register_bits(mdp, ComIEnReg,
-                        (1 << ComIEnReg_RxIEn) |
-                        (1 << ComIEnReg_ErrIEn));
-    mfrc522_write_register(mdp, ComIrqReg, 0xFF & ~(1 << ComIrqReg_Set1));
+    cleanup_transceive(mdp);
     mdp->state = PCD_READY;
-
-    if (message == MSG_TIMEOUT) {
-        return PCD_OK_TIMEOUT;
-    }
-
-    if (message != MFRC522_MSG_INTERRUPT) {
-        // OK, this should not have happened. Fail-fast.
-        osalSysHalt("Unexpected wakeup message");
-    }
-
-    // Handle possible error
-    if (!(mfrc522_read_register(mdp, ComIrqReg) & (1 << ComIrqReg_RxIRq))) {
-        // This was not a 'receive complete' interrupt
-        // TODO more thorough error handling
-        return PCD_ERROR;
-    }
-
-    mdp->resp_read_bytes = 0;
-    mdp->resp_length = mfrc522_read_register(mdp, FIFOLevelReg);
-    *resp_length = mdp->resp_length;
-    mfrc522_read_register_burst(mdp, FIFODataReg, mdp->response, mdp->resp_length);
-
-    return PCD_OK;
+    return handle_response(mdp, message, resp_length, false);
 }
 
+// TODO untested!
 pcdresult_t mfrc522TransceiveStandardFrameA(void *inst, uint8_t *buffer,
                                             uint16_t length,
                                             uint16_t *resp_length,
@@ -197,79 +223,47 @@ pcdresult_t mfrc522TransceiveStandardFrameA(void *inst, uint8_t *buffer,
     DEFINE_AND_SET_mdp(inst);
     CHECK_STATE(mdp->state != PCD_READY);
 
-    // TODO DRY!
-
     mdp->state = PCD_ACTIVE;
+    prepare_transceive(mdp);
 
-    // Flush FIFO
-    mfrc522_write_register(mdp, FIFOLevelReg, (1 << FIFOLevelReg_FlushBuffer));
-
-    // Write data
+    // Write data, set Start Send
     mfrc522_write_register_burst(mdp, FIFODataReg, buffer, length);
+    mfrc522_write_register(mdp, BitFramingReg,
+                           (7 << BitFramingReg_TxLastBits) |
+                           (1 << BitFramingReg_StartSend));
 
-    // Clear interrupt bits
-    // Select all interrupts, bit Set1 is zero. This will unset all selected
-    // interrupts.
-    mfrc522_write_register(mdp, ComIrqReg, 0xFF & ~(1 << ComIrqReg_Set1));
-    mdp->interrupt_pending = false;
-    // Enable interrupts: Rx complete, Timeout and Error
-    mfrc522_set_register_bits(mdp, ComIEnReg,
-                      (1 << ComIEnReg_RxIEn) |
-                      (1 << ComIEnReg_ErrIEn));
+    msg_t message = wait_for_response(mdp, timeout_us);
 
-
-    mfrc522_command(mdp, Command_Transceive);
-    // Start Send
-    mfrc522_write_register(mdp, BitFramingReg, (1 << BitFramingReg_StartSend));
-
-    osalSysLock();
-    msg_t message;
-    if (!mdp->interrupt_pending) {
-        message = osalThreadSuspendTimeoutS(&mdp->tr,
-                                            OSAL_US2ST(timeout_us));
-    }
-    mdp->interrupt_pending = false;
-    osalSysUnlock();
-
-    // Reset bit-oriented adjustments and clear 'Start Send' flag
-    mfrc522_write_register(mdp, BitFramingReg, 0);
-
-    // Clear interrupt bits and disable previously enabled interrupts
-    mfrc522_clear_register_bits(mdp, ComIEnReg,
-                        (1 << ComIEnReg_RxIEn) |
-                        (1 << ComIEnReg_ErrIEn));
-    mfrc522_write_register(mdp, ComIrqReg, 0xFF & ~(1 << ComIrqReg_Set1));
-
-    if (message == MSG_TIMEOUT) {
-        return PCD_OK_TIMEOUT;
-    }
-
-    if (message != MFRC522_MSG_INTERRUPT) {
-        // OK, this should not have happened. Fail-fast.
-        osalSysHalt("Unexpected wakeup message");
-    }
-
-    // Handle possible error
-    if (!(mfrc522_read_register(mdp, ComIrqReg) & (1 << ComIrqReg_RxIRq))) {
-        // This was not a 'receive complete' interrupt
-        // TODO more thorough error handling
-        return PCD_ERROR;
-    }
-
-    mdp->resp_read_bytes = 0;
-    mdp->resp_length = mfrc522_read_register(mdp, FIFOLevelReg);
-    *resp_length = mdp->resp_length;
-    mfrc522_read_register_burst(mdp, FIFODataReg, mdp->response, mdp->resp_length);
-
-    return PCD_OK;
+    cleanup_transceive(mdp);
+    mdp->state = PCD_READY;
+    return handle_response(mdp, message, resp_length, false);
 }
 
+// TODO untested!
 pcdresult_t mfrc522TransceiveAnticollFrameA(void *inst, uint8_t *buffer,
                                             uint16_t length,
                                             uint8_t n_last_bits,
                                             uint16_t *resp_length,
                                             uint32_t timeout_us) {
+    MEMBER_FUNCTION_CHECKS(inst);
+    DEFINE_AND_SET_mdp(inst);
+    CHECK_STATE(mdp->state != PCD_READY);
 
+    mdp->state = PCD_ACTIVE;
+    prepare_transceive(mdp);
+
+    // Write data, set Start Send and set number of valid bytes.
+    // TODO completly forgot about RX align!
+    mfrc522_write_register_burst(mdp, FIFODataReg, buffer, length);
+    mfrc522_write_register(mdp, BitFramingReg,
+                           ((n_last_bits & 0x7) << BitFramingReg_TxLastBits) |
+                           (1 << BitFramingReg_StartSend));
+
+    msg_t message = wait_for_response(mdp, timeout_us);
+
+    cleanup_transceive(mdp);
+    mdp->state = PCD_READY;
+    return handle_response(mdp, message, resp_length, true);
 }
 
 uint16_t mfrc522GetResponseLengthA(void *inst) {
