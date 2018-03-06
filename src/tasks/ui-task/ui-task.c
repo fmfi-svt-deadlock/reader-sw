2f4bf27cf121ea5ff6bcf7ec6203aacad1590dbf
Adam Dej <dejko.a@gmail.com>
2018-03-06 17:39:17 +0100

    [UITask] Implement state switch

--- src/tasks/ui-task/ui-task.c
+++ src/tasks/ui-task/ui-task.c
@@ -32,7 +32,9 @@ static thread_t                 *task_thread;
 /* Packs a message to msg_t. msg_t is guaranteed to be able to hold pointer on the target
  * architecture, which is 32 bits on MCUs we are using.
  */
-#define PACK_MESSAGE(msg_type, msg_payload)     ((msg_t)((msg_type >> 16) | msg_payload))
+#define PACK_MESSAGE(msg_type, msg_payload)     ((msg_t)((msg_type << 16) | msg_payload))
+#define UNPACK_TYPE(msg)                        ((msg & 0xFFFF0000) >> 16)
+#define UNPACK_PAYLOAD(msg)                     (msg & 0xFFFF)
 #define MSG_SET_STATE                           1
 #define MSG_FLASH                               2
 
@@ -53,12 +55,35 @@ void dlTaskUiFlashMessage(dl_task_ui_flash flash) {
 
 static THD_FUNCTION(uiTask, arg) {
     (void) arg;
+    dl_task_ui_state state = DL_TASK_UI_STATE_ERROR;
+    msg_t msg;
+    msg_t fetch_status;
 
     while (!chThdShouldTerminateX()) {
-        palToggleLine(LINE_LED_STATUS_R);
-        chThdSleepMilliseconds(500);
-    }
+        fetch_status = chMBFetch(&inbox, &msg, OSAL_MS2ST(150));
+        if (fetch_status == MSG_OK /*&& UNPACK_TYPE(msg) == MSG_SET_STATE*/) {
+            state = UNPACK_PAYLOAD(msg);
+            // Clear previous LED states
+            palClearLine(LINE_LED_STATUS_R);
+            palClearLine(LINE_LED_STATUS_G);
+            palClearLine(LINE_LED_LOCK_R);
+            palClearLine(LINE_LED_LOCK_G);
+        }
 
+        switch(state) {
+            case DL_TASK_UI_STATE_ERROR:
+                palToggleLine(LINE_LED_STATUS_R);
+                break;
+            case DL_TASK_UI_STATE_LOCKED:
+                palSetLine(LINE_LED_STATUS_G);
+                palSetLine(LINE_LED_LOCK_R);
+                break;
+            case DL_TASK_UI_STATE_UNLOCKED:
+                palSetLine(LINE_LED_STATUS_G);
+                palSetLine(LINE_LED_LOCK_G);
+                break;
+        }
+    }
 }
 
 
